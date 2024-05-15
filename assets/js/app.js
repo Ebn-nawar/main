de295fdc86fd9119645dfcd2aac2694e9cf4637b
ebn-nawar <ebn.nawar2002@gmail.com>
2024-05-15 06:29:40 +0300

    edits

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,32 @@
+const contactForm = document.forms["contactForm"],
+  inputName = contactForm["fullName"],
+  inputEmail = contactForm["email"],
+  inputPhoneNumber = contactForm["phoneNumber"],
+  inputMsg = contactForm["msg"],
+  inputService = contactForm["service"],
+  contactFormElements = [inputName , inputEmail , inputPhoneNumber , inputMsg, inputService]
+
+  contactForm.addEventListener("submit" , function(e){
+    contactFormElements.map( x => x.value === "" ? e.preventDefault() : false )
+    const fullNamePattern = /[a-z]\s[a-z]\s[a-z]/
+    if (inputName.value === "") {
+      alert("لايجب ان يكون الاسم خاليا")
+    }else if(!(fullNamePattern.test(inputName.value))){
+      alert("يجب ان يحتوي الاسم علي حروف فقط")
+      
+    }
+  })
+  
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function () {
   var owl = $(".owl-carousel");
   owl.owlCarousel({
